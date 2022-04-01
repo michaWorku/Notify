6c3515e54620e7eb879973806498e90f8811211f
michaworku <mikias27worku@gmail.com>
2022-04-01 14:27:29 +0300

    file and folder structure

---
+++ client/src/componetns/Post/Post.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Post.scss'
+
+const Post = () => {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Navbar, Post } from '../../componetns'
+import { useAuthContext } from '../../context/auth/authContext';
 
 const Home = () => {
+  const { currentUser } = useAuthContext().state;
   return (
-    <div>Home</div>
+    <div className='container'>
+      <Navbar/>
+      <Post/>
+      <span className="username">{currentUser}</span>
+    </div>
   )
 }
 
